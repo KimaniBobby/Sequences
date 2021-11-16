bab6d2f8619bd80b3ecdc7eb6b14d6e08b04989a
KimaniBobby <90858318+KimaniBobby@users.noreply.github.com>
2021-11-16 11:50:18 +0300

    Different line
    This program prompts the user to enter two numbers and prints them out on different lines

---
+++ diffline.py
@@ -0,0 +1,14 @@
+def main():
+    a = str(input("Please enter the first number:"))
+    b = str(input("Please enter the second number:"))
+
+    
+
+    for i in a:
+        print(i)
+
+    for c in b:
+        print(c)
+
+    
+main()
