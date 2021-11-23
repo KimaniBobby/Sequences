924e81e24d599c3ebd66da830410696cdc3e78a7
Bobby Kimani <kimani.bobby@gmail.com>
2021-11-23 16:08:53 +0300

    a program that reads values between 1 and 5 and assigns a grade

---
+++ grade.py
@@ -0,0 +1,16 @@
+
+def main():
+    x = int(input("Please enter values between 1 and 5:"))
+
+    if x ==1:
+        print("Fail")
+    if x == 2:
+        print("Poor")
+    if x== 3:
+        print("Fair")
+    if x ==4:
+        print("Good")
+    elif x==5:
+        print("Excellent")
+main()                
+
