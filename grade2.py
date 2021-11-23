0545eb0d7627dd3160f22395f752a93a7e2813eb
Bobby Kimani <kimani.bobby@gmail.com>
2021-11-23 16:10:45 +0300

    a program that reads a value from the user between 1 and 100 and assigns a grade

---
+++ grade2.py
@@ -0,0 +1,16 @@
+
+def main():
+    x = int(input("Please enter values between 1 and 100:"))
+
+    if x >90 and x<100:
+        print("A")
+    if x>80 and x<90:
+        print("B")
+    if x>70 and x< 80:
+        print("C")
+    if x >60 and x<70:
+        print("D")
+    elif x >1 and x<60:
+        print("F")
+main()                
+
